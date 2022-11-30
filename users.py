612444d4fcbf1efa99b332d22f8e6f035272eff9
Fabricio Catae <fcatae@hotmail.com>
2022-11-30 20:47:27 -0300

    add api contract

--- users.py
+++ users.py
@@ -1,17 +1,45 @@
-from flask import Flask, redirect, url_for
+from flask import Flask, request, redirect
 app = Flask(__name__)
 
 @app.route('/authenticate')
 def api_auth():
-   return {'msg': 'test: auth'}
+    content = request.get_json()
+    username = content['username']
+    password = content['password']
+
+    return {
+            'ID': 1,
+            'Authenticated': True,
+            'UserId': 123,
+            'Token': 'abcde',
+            'SessionToken': 'abcdefgh',
+            'AccountId': '321',
+            'OMSId': '1'
+        }
+
+@app.route('/Validar')
+def api_validate():
+    return {
+            'result': True
+        }
 
 @app.route('/LogOut')
 def api_logout():
-   return {'msg': 'test: logout'}
+    session = ''
+    return { 
+            'result': True,
+            'errormsg': 'null',
+            'errorcode': 0,
+            'detail': 'null'
+        }
 
-@app.route('/register/user')
+@app.route('/register/user', methods = ['POST'])
 def api_register_user():
-   return {'msg': 'test: register uses'}
+    content = request.get_json()
+    username = content['customerCredential']['username']
+    email = content['contactInfo']['email']
+
+    return { 'msg': f'User registered: {username} ({email})' }
 
 if __name__ == '__main__':
    app.run(debug = True)
