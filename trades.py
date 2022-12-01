9904b00647153315e44f9f18a3456d49885a7ffe
Fabricio Catae <fcatae@hotmail.com>
2022-11-30 22:04:50 -0300

    add trades

--- trades.py
+++ trades.py
@@ -1,10 +1,13 @@
 from flask import Flask, redirect, url_for
 app = Flask(__name__)
+properties = { 'transaction_id': 1000}
 
-@app.route('/exchange/trades/<oms_id>/account/<account_id>/order')
+@app.route('/exchange/trades/<oms_id>/account/<account_id>/order', methods=['GET', 'POST'])
 def api_trades_account_order(oms_id, account_id):
+   properties['transaction_id'] += 1
    return {
-         'msg': 'test: trades',
+         'msg': 'success',
+         'id': properties['transaction_id'] ,
          'oms': oms_id,
          'acc': account_id
       }
